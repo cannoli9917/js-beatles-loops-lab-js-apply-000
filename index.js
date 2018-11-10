78edf2a9e4d693b0bf329521a2c830d2f3384851
Katie Pennachio <katie.pennachio@gmail.com>
2018-11-10 19:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesList = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesList.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatlesList;
+}
+
+function johnLennonFacts(facts) {
+  var factList = [];
+  for (var i = 0; i < facts.length; i++){
+    factList.push(`${facts[i]}!!!`);
+  }
+  return factList;
+ }
